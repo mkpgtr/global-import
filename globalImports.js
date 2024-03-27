cedcdb2c7f414828a10f8477bc49bb388b5d3b38
Manish Panda <mkpgtr@gmail.com>
2024-03-28 01:05:20 +0530

    add useState & reactLogo to globalImports.js

--- globalImports.js
+++ globalImports.js
@@ -1,5 +1,9 @@
 // globalImports.js
 import { toast } from 'react-toastify';
+import reactLogo from './src/assets/react.svg'  
+import { useState } from 'react'
 
 // Attach toast to the global/window object
 globalThis.toast = toast;
+globalThis.reactLogo = reactLogo
+globalThis.useState = useState
